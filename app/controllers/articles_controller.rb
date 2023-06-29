5c84160bf4996058f40906ce3950556e117e08c2
Kamania <kamaniajoseph@gmail.com>
2023-06-29 10:34:35 +0300

    Save new article to database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
   def index
@@ -16,23 +15,15 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  # GET /articles/1/edit
-  def edit
-  end
-
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+  # GET /articles/1/edit
+  def edit
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
